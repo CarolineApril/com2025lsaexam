00c8be29cd07f8713b108b57798d7f3e9d9665d4
Caroline Selwood <carolineselwood@outlook.com>
2018-08-28 15:46:48 +0000

    headers&footers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :thoughts
   resources :topics
   get 'home/index'
   
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180828145518) do
+ActiveRecord::Schema.define(version: 20180828153201) do
+
+  create_table "thoughts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "shared"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "topics", force: :cascade do |t|
     t.string   "title"
